542a19ccefc65fa5ce72db4c50f7b7cc845ea42f
tomp47 <103162739+tomp47@users.noreply.github.com>
2023-06-04 11:06:00 -0400

    Update ica8.js

--- ica/ica8/ica8.js
+++ ica/ica8/ica8.js
@@ -1,9 +1,12 @@
 const btn = document.querySelector("#button");
 // console.log(btn)
+const paragraph = document.querySelector("p");
+
 
 btn.onclick = () => {
-    const color= prompt("What color would you like the  box to be?")
+    const color = prompt("What color would you like the  box to be?")
+    alert(`You changed the color  to ${color}!`)
+    paragraph.style.backgroundColor = color;
 }
 
 
-let p = color;
