9903f005da7ffe73575a5adec27cc41a60ffc7d1
tomp47 <103162739+tomp47@users.noreply.github.com>
2023-06-04 10:55:46 -0400

    ica8

---
+++ ica/ica8/ica8.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("#button");
+// console.log(btn)
+
+btn.onclick = () => {
+    const color= prompt("What color would you like the  box to be?")
+}
+
+
+let p = color;
