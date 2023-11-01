be8190983ba791df541000d2f9af42a311bfb24e
tomp47 <103162739+tomp47@users.noreply.github.com>
2023-11-01 12:52:13 -0600

    new portfolio

---
+++ portfolio/portfolio.js
@@ -0,0 +1,6 @@
+// You can add JavaScript functionality here, such as form submission handling.
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Handle form submission, e.g., send data to a server or show a confirmation message.
+    alert("Form submitted!");
+});
